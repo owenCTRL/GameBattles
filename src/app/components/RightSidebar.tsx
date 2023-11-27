14d710ee94083df0fcc0647bbe379b0e00f17d93
owen <35806309+owenAPI@users.noreply.github.com>
2023-11-27 14:38:33 -0500

    initial ui complete

--- src/app/components/RightSidebar.tsx
+++ src/app/components/RightSidebar.tsx
@@ -3,7 +3,7 @@ import { BsSearch } from "react-icons/bs";
 const RightSidebar = () => {
   return (
     <section className="w-[30%] mt-2 flex flex-col items-stretch h-fit-content px-6">
-      <div className="backdrop-blur bg-black/10 sticky top-2">
+      <div className="backdrop-blur bg-black/10 sticky top-0">
         <div className="relative w-full h-full">
           <input
             id="searchBox"
@@ -21,21 +21,27 @@ const RightSidebar = () => {
       </div>
       <div>
         <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
-          <h3 className="font-bold text-2xl my-4 p-4">Global Rankings</h3>
+          <h3 className="font-bold text-xl my-2 px-4">Global Rankings</h3>
           <div>
-            {Array.from({ length: 5 }).map((_, i) => (
-              <div key={i} className="hover:bg-white/10 p-4">
-                <div className="font-bold text-lg">Team ${i + 1}</div>
+            {Array.from({ length: 3 }).map((_, i) => (
+              <div
+                key={i}
+                className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"
+              >
+                <div className="font-bold text-lg">Team {i + 1}</div>
                 <div className="text-xs text-neutral-400">ELO</div>
               </div>
             ))}
           </div>
         </div>
         <div className="flex flex-col rounded-xl bg-neutral-900 my-4">
-          <h3 className="font-bold text-2xl my-4 p-4">Regional Leagues</h3>
+          <h3 className="font-bold text-xl my-2 px-4">Regional Leagues</h3>
           <div>
-            {Array.from({ length: 5 }).map((_, i) => (
-              <div key={i} className="hover:bg-white/10 p-4">
+            {Array.from({ length: 3 }).map((_, i) => (
+              <div
+                key={i}
+                className="hover:bg-white/10 p-4 last:rounded-b-xl transition duration-200"
+              >
                 <div className="font-bold text-lg">League {i + 1}</div>
                 <div className="text-xs text-neutral-400">Location</div>
               </div>
